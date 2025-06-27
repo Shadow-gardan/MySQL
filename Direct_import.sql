9ae9a0b5b038f2435d125f52c9f6c0895100ec44
Aman singh rawat <ar6477594@gmail.com>
2025-06-27 08:56:29 +0530

    Direct Import

---
+++ Direct_import.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE school;
+USE school;
+
+-- click database file
+-- right click on file
+-- select the import or export CSV file
+-- enable the header file 
+-- chose the path of the file
+-- do next,nextand fines 
+
+
+select* from marks;
